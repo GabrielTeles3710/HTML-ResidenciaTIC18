1eb3762cf9309b30662430c4ce5ede8a3b76dc56
LobinhoTroll <gabrielteles931@gmail.com>
2024-04-01 14:52:07 -0300

    parte de puxar dados concluida

--- Modulo avancado/FEA-006/src/app/readjson/readjson.component.ts
+++ Modulo avancado/FEA-006/src/app/readjson/readjson.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { map } from 'rxjs';
+import { Form } from '@angular/forms';
 
 /* 
 https://dontpad.com/lobinhofront
@@ -15,15 +16,15 @@ export class ReadjsonComponent implements OnInit {
 constructor(private http: HttpClient){
 };
 countries: any = [];
+campos_dos_form: any = [{tipo: "text", nome: "nome",rotulo: "rotulo"}];
 url: string = "https://restcountries.com/v3.1/all";
 
 getCountries(){
  return this.http.get<any[]>(this.url).pipe(map(data => data));
 }
-
 ngOnInit() {    
-  //this.http.get(this.url).subscribe(countries => console.log(countries));
-  this.getCountries().subscribe(countries => this.countries = Object.keys(countries))
+  this.http.get(this.url).subscribe(countries => console.log(countries));
+  this.getCountries().subscribe(countries => {this.countries = Object.keys(countries[0]); console.log(Object.keys(countries[0]))});
 }
 }
 
